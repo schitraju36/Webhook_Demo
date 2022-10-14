d3281d22db8864a1fc833ba1c475e214e7dd7344
CG Santhosh <schitraju36@gmail.com>
2022-10-14 12:58:52 +0530

    added for webhook

--- src/main/java/com/example/webhook/controller/WebHookController.java
+++ src/main/java/com/example/webhook/controller/WebHookController.java
@@ -21,19 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class WebHookController {
-	
-	 @Value("${TOKEN}")
-	 private String token;
-	 
-	 @Value("${MYTOKEN}")
-	 private String myToken;
-	
+
+	@Value("${TOKEN}")
+	private String token;
+
+	@Value("${MYTOKEN}")
+	private String myToken;
+
 	@PostMapping // http://localhost:8080/api/webhook
-    public ResponseEntity<String> print(@RequestBody String requestBody) {
-        System.out.println("###### Webhook #####" + requestBody);
-        return new ResponseEntity<String >(requestBody, HttpStatus.OK);
-    }
-	
+	public ResponseEntity<String> print(@RequestBody String requestBody) {
+		System.out.println("###### Webhook #####" + requestBody);
+		return new ResponseEntity<String>(requestBody, HttpStatus.OK);
+	}
+
 	/*
 	 * @GetMapping("/webhooks/{mode}/{challenge}/{token}") public
 	 * ResponseEntity<String> webHook(@RequestParam("mode") String
@@ -43,27 +43,24 @@ public class WebHookController {
 	 * 
 	 * return new ResponseEntity<String >(mode, HttpStatus.OK); }
 	 */
-	
+
 	@GetMapping("/webhooks")
-    public ResponseEntity<String> webHook1(HttpServletRequest request, HttpServletResponse httpResp) {
-		String hubMode=request.getParameter("hub.mode");
-		String challenge=request.getParameter("hub.challenge");
-		String token=request.getParameter("hub.verify_token");
-		System.out.println("hubMode :" + hubMode +"challenge :" + challenge + "verify_token :"+ token);
-        //System.out.println(request.getRequestURI() + "?" + request.getQueryString());
-      //  String myToken=null;	
-        
-        if(StringUtils.isNoneEmpty(hubMode) &&  StringUtils.isNoneEmpty(token)) {
-        	
-        	if( hubMode.equals("subscrbe") && token.equals(myToken)) {
-        		return new ResponseEntity<String >(challenge, HttpStatus.OK);
-        		
-        	} else{
-        		return new ResponseEntity<String >(challenge, HttpStatus.FORBIDDEN);
-        	}
-        	        	
-        }else {
-        	return new ResponseEntity<String >(challenge, HttpStatus.BAD_REQUEST);
-        }       
+	public ResponseEntity<String> webHook1(HttpServletRequest request, HttpServletResponse httpResp) {
+		String hubMode = request.getParameter("hub.mode");
+		String challenge = request.getParameter("hub.challenge");
+		String token = request.getParameter("hub.verify_token");
+		System.out.println("hubMode :" + hubMode + "challenge :" + challenge + "verify_token :" + token);
+
+		if (StringUtils.isNoneEmpty(hubMode) && StringUtils.isNoneEmpty(token)) {
+			if (hubMode.equals("subscrbe") && token.equals(myToken)) {
+				return new ResponseEntity<String>(challenge, HttpStatus.OK);
+
+			} else {
+				return new ResponseEntity<String>(challenge, HttpStatus.FORBIDDEN);
+			}
+
+		} else {
+			return new ResponseEntity<String>(challenge, HttpStatus.BAD_REQUEST);
+		}
 	}
 }
